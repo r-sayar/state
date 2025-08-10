7ff2169c44d21d130f422c6ccb998532f287f444
aceniccola <andrewsamce@me.com>
2025-08-09 23:01:44 -0400

    pipeline and fewshot changes

--- scripts/fullpipe_andrew.sh
+++ scripts/fullpipe_andrew.sh
@@ -6,62 +6,44 @@
 MODEL_DIR="competition"
 
 # experiment name
-NAME="training_finetune_andrew"
-
-# old checkpoint name
-# CKPT="step30000l1d063.ckpt"
+DIR_NAME="training_finetune_andrew1"
 
 # toml config path
-TOML_CONFIG="examples/andrewfewshot.toml"
-
-# number of workers
-NUM_WORKERS=8
+TOML_CONFIG="examples/andrew_fewshot.toml"
 
 # perturbation features file
 PERT_FEATURES="competition_support_set/ESM2_pert_features.pt"
 
+# prediction file name
+PREDICTION_NAME="prediction"
+
+# checkpoint file name
+CKPT="final.ckpt"
+
+# output directory for results
+OUT_DIR=cell-eval-outdir
+
+# parallelization
+THREADS=8
+NUM_WORKERS=8
+BATCH_SIZE=100
 
 # -- Activate working environment --
-# TODO:Activate working environment
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate mne
 
 # Exit on error
 set -e
+export OMP_NUM_THREADS=$THREADS VECLIB_MAXIMUM_THREADS=$THREADS
+export HDF5_USE_FILE_LOCKING=FALSE
 
-# Run the training command
-uv run state tx train \
-  data.kwargs.toml_config_path="${TOML_CONFIG}" \
-  data.kwargs.num_workers="${NUM_WORKERS}" \
-  data.kwargs.batch_col="batch_var" \
-  data.kwargs.pert_col="target_gene" \
-  data.kwargs.cell_type_key="cell_type" \
-  data.kwargs.control_pert="non-targeting" \
-  data.kwargs.perturbation_features_file="${PERT_FEATURES}" \
-  training.max_steps=1000 \
-  training.ckpt_every_n_steps=500 \
-  training.val_freq=100 \
-  model=state_sm \
-  model.kwargs.nb_decoder=true \
-  wandb.tags="[${NAME}]" \
-  output_dir="${MODEL_DIR}" \
-  name="${NAME}" \
-  use_wandb=false
 
-# -- Predict --
-# gets metrics.csv along with real and predicted adata from test holdouts
-uv run state tx predict \
-    --checkpoint "final.ckpt" \
-    --output_dir "${MODEL_DIR}/${NAME}/" \
-    --profile full
 
-# -- Infer --
-# gets prediction.h5ad for the competition submission
-uv run state tx infer \
-  --output "${MODEL_DIR}/${NAME}/prediction.h5ad" \
-  --model_dir "${MODEL_DIR}/${NAME}" \
-  --checkpoint "${MODEL_DIR}/${NAME}/checkpoints/${CKPT}" \
-  --adata "competition_support_set/competition_val_template.h5ad" \
-  --pert_col "target_gene"
+uv run -m cell_eval score \
+    -i ${OUT_DIR}/cell-eval-outdir-results/agg_results.csv \
+    -I ${OUT_DIR}/cell-eval-outdir-baseline/agg_results.csv \
+    -o ${OUT_DIR}/baseline_diff_test.csv
 
-# -- Gets prediction.vcc --
+echo "#### Running cell-eval prep ####"
 # remember to have `sudo apt install -y zstd` before running this
-uv tool run --from git+https://github.com/ArcInstitute/cell-eval@main cell-eval prep -i ${MODEL_DIR}/${NAME}/prediction.h5ad -g competition_support_set/gene_names.csv
+uv tool run --from git+https://github.com/ArcInstitute/cell-eval@main cell-eval prep -i ${MODEL_DIR}/${DIR_NAME}/${PREDICTION_NAME}.h5ad -g competition_support_set/gene_names.csv
