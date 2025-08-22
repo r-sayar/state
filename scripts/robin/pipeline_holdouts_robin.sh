fefe0ccc1a3bda44125849cf584d8ff6bbb98b83
RLS <robin.sayar@fu-berlin.de>
2025-08-22 10:57:08 +0200

    cp-ing toml

--- scripts/robin/pipeline_holdouts_robin.sh
+++ scripts/robin/pipeline_holdouts_robin.sh
@@ -2,18 +2,22 @@
 # infering for the competition submission and getting the .vcc file
 
 # -- Config --
-# Path to your trained model directory
-MODEL_DIR="competition_experiments"
+# Path to your trained model directory where your models are
+MODEL_DIR="results/"
 
 # experiment name
-DIR_NAME="training_finetune_andrew3"
+DIR_NAME="2_layer_transformer_esm2"
 
 # toml config path
-TOML_CONFIG="examples/andrew_fewshot.toml"
+TOML_CONFIG="scripts/robin/all_data.toml"
+
+# Competition support set
+COMPETITION_SUPPORT_SET="datasets/base_dataset"
 
 # perturbation features file
-PERT_FEATURES="competition_support_set/ESM2_pert_features.pt"
-
+PERT_FEATURES="datasets/embeddings/ESM2_pert_features.pt"
+#"datasets/embeddings/GenePT_gene_embedding_ada_text.pt"
+#datasets/embeddings/ESM2_pert_features.pt
 # prediction file name
 PREDICTION_NAME="prediction"
 
@@ -21,7 +25,7 @@ PREDICTION_NAME="prediction"
 CKPT="final.ckpt"
 
 # output directory for results
-OUT_DIR=cell-eval-outdir
+OUT_DIR=results/pipeline_holdouts_robin
 
 # parallelization
 THREADS=8
@@ -52,6 +56,7 @@ fi
 
 cp -a "$SRC_ABS" "$DEST_DIR/" || cp -a "$SRC" "$DEST_DIR/"
 
+
 # copy toml config into the model directory for reproducibility
 if [ -e "$TOML_CONFIG" ]; then
   cp -a "$TOML_CONFIG" "$DEST_DIR/$(basename "$TOML_CONFIG")"
@@ -59,12 +64,9 @@ else
   echo "Warning: TOML_CONFIG '$TOML_CONFIG' does not exist. Skipping copy." >&2
 fi
 
-# -- Activate working environment --
-source ~/miniforge3/etc/profile.d/conda.sh
-conda activate mne
-
 echo "#### Running training ####"
 
+#use learning rate 1e-5
 uv run state tx train \
   data.kwargs.toml_config_path=${TOML_CONFIG} \
   data.kwargs.num_workers=${NUM_WORKERS} \
@@ -73,7 +75,7 @@ uv run state tx train \
   data.kwargs.cell_type_key=cell_type \
   data.kwargs.control_pert=non-targeting \
   data.kwargs.perturbation_features_file=${PERT_FEATURES} \
-  training.max_steps=2000 \
+  training.max_steps=40000 \
   training.ckpt_every_n_steps=500 \
   training.val_freq=250 \
   model=state_sm \
@@ -81,9 +83,12 @@ uv run state tx train \
   wandb.tags=[${DIR_NAME}] \
   output_dir=${MODEL_DIR} \
   name=${DIR_NAME} \
+  training.lr=1e-5 \
   use_wandb=false
 
-echo "### Generating holdout ground truth ###"
+#model.kwargs.transformer_backbone_kwargs.num_hidden_layers=2 \
+
+
 python scripts/prepare_holdout_ground_truth.py \
   --toml_config ${TOML_CONFIG} \
   --split test \
@@ -94,10 +99,10 @@ python scripts/prepare_holdout_ground_truth.py \
 echo "#### Running cell-eval baseline ####"
 
 uv run -m cell_eval baseline \
-    -a ${OUT_DIR}/${DIR_NAME}/holdout_ground_truth_test.h5ad \
-    -c ${OUT_DIR}/${DIR_NAME}/holdout_counts_test.csv \
-    -o ${OUT_DIR}/${DIR_NAME}/baseline_test.h5ad \
-    -O ${OUT_DIR}/${DIR_NAME}/baseline_de_test.csv \
+    -a ${OUT_DIR}/holdout_ground_truth_test.h5ad \
+    -c ${OUT_DIR}/holdout_counts_test.csv \
+    -o ${OUT_DIR}/baseline_test.h5ad \
+    -O ${OUT_DIR}/baseline_de_test.csv \
     --pert-col target_gene \
     --control-pert non-targeting \
     --num-threads ${THREADS} 
@@ -107,7 +112,7 @@ echo "#### Running prediction ####"
 # gets metrics.csv along with real and predicted adata from test holdouts
 uv run state tx predict \
     --checkpoint "final.ckpt" \
-    --output_dir ${MODEL_DIR}/${DIR_NAME} \
+    --output_dir "${MODEL_DIR}/${DIR_NAME}/" \
     --profile full
 
 echo "#### Running inference ####"
@@ -117,7 +122,7 @@ uv run state tx infer \
   --output ${MODEL_DIR}/${DIR_NAME}/${PREDICTION_NAME}.h5ad \
   --model_dir ${MODEL_DIR}/${DIR_NAME} \
   --checkpoint ${MODEL_DIR}/${DIR_NAME}/checkpoints/${CKPT} \
-  --adata ${OUT_DIR}/${DIR_NAME}/holdout_ground_truth_test.h5ad \
+  --adata ${OUT_DIR}/holdout_ground_truth_test.h5ad \
   --pert_col target_gene
 
 echo "#### Running cell-eval run ####"
@@ -125,23 +130,21 @@ echo "#### Running cell-eval run ####"
 # run cell-eval on the holdout predictions
 uv run -m cell_eval run \
     -ap ${MODEL_DIR}/${DIR_NAME}/${PREDICTION_NAME}.h5ad \
-    -ar ${OUT_DIR}/${DIR_NAME}/holdout_ground_truth_test.h5ad \
-    -o ${OUT_DIR}/${DIR_NAME}/cell-eval-outdir-results \
+    -ar ${OUT_DIR}/holdout_ground_truth_test.h5ad \
+    -o ${OUT_DIR}/cell-eval-outdir-results \
     --pert-col target_gene \
     --control-pert non-targeting \
-    --profile vcc \
     --skip-metrics pearson_edistance,clustering_agreement,discrimination_score_l2,discrimination_score_cosine \
     --num-threads ${THREADS} \
     --batch-size ${BATCH_SIZE}
 
 # run cell-eval on the baseline predictions
 uv run -m cell_eval run \
-    -ap ${OUT_DIR}/${DIR_NAME}/baseline_test.h5ad \
-    -ar ${OUT_DIR}/${DIR_NAME}/holdout_ground_truth_test.h5ad \
-    -o ${OUT_DIR}/${DIR_NAME}/cell-eval-outdir-baseline \
+    -ap ${OUT_DIR}/baseline_test.h5ad \
+    -ar ${OUT_DIR}/holdout_ground_truth_test.h5ad \
+    -o ${OUT_DIR}/cell-eval-outdir-baseline \
     --pert-col target_gene \
     --control-pert non-targeting \
-    --profile vcc \
     --skip-metrics pearson_edistance,clustering_agreement,discrimination_score_l2,discrimination_score_cosine \
     --num-threads ${THREADS} \
     --batch-size ${BATCH_SIZE}
@@ -149,18 +152,18 @@ uv run -m cell_eval run \
 echo "#### Running cell-eval score ####"
 
 uv run -m cell_eval score \
-    -i ${OUT_DIR}/${DIR_NAME}/cell-eval-outdir-results/agg_results.csv \
-    -I ${OUT_DIR}/${DIR_NAME}/cell-eval-outdir-baseline/agg_results.csv \
-    -o ${OUT_DIR}/${DIR_NAME}/baseline_diff_test.csv
+    -i ${OUT_DIR}/cell-eval-outdir-results/agg_results.csv \
+    -I ${OUT_DIR}/cell-eval-outdir-baseline/agg_results.csv \
+    -o ${OUT_DIR}/baseline_diff_test.csv
 
 # gets prediction.h5ad for the holdout predictions
 uv run state tx infer \
   --output ${MODEL_DIR}/${DIR_NAME}/competition_val_prediction.h5ad \
   --model_dir ${MODEL_DIR}/${DIR_NAME} \
   --checkpoint ${MODEL_DIR}/${DIR_NAME}/checkpoints/${CKPT} \
-  --adata competition_support_set/competition_val_template.h5ad \
+  --adata ${COMPETITION_SUPPORT_SET}/competition_val_template.h5ad \
   --pert_col target_gene
 
 echo "#### Running cell-eval prep ####"
-# remember to have `sudo apt install -y zstd` before running this
-uv tool run --from git+https://github.com/ArcInstitute/cell-eval@main cell-eval prep -i ${MODEL_DIR}/${DIR_NAME}/competition_val_prediction.h5ad -g competition_support_set/gene_names.csv
+# # remember to have `sudo apt install -y zstd` before running this
+# uv tool run --from git+https://github.com/ArcInstitute/cell-eval@main cell-eval prep -i ${MODEL_DIR}/${DIR_NAME}/competition_val_prediction.h5ad -g ${COMPETITION_SUPPORT_SET}/gene_names.csv
